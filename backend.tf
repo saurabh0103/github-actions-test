afb7d0ac8d2807aee7268286aed2bbc7b22a51b7
Saurabh Pandey <saurabh.pandey@tredence.com>
2022-05-10 17:03:57 +0530

    terra5

--- backend.tf
+++ backend.tf
@@ -11,15 +11,3 @@ terraform {
 }
 
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.13.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
