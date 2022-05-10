090e4252e793122d3d34df6ffa0cf435afe6b509
Saurabh Pandey <saurabh.pandey@tredence.com>
2022-05-10 17:28:21 +0530

    terra6

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.13.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
 
 terraform {
   backend "remote" {
