f89c02cb1081a79d1e431da9381b585784572c22
Saurabh Pandey <saurabh.pandey@tredence.com>
2022-05-10 16:58:35 +0530

    terra3

--- backend.tf
+++ backend.tf
@@ -11,7 +11,16 @@ terraform {
 }
 
 
- provider "aws" {
-    region= "ap-south-1"
-    
- }
+ terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
